2d05cd2a468f94c89a5b1b5fbce2208b7b0f83d8
Joseph Smith <joseph.smit95@gmail.com>
2024-02-12 10:31:34 -0500

    added notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,26 @@
-class UsersController < ApplicationController\
+# Handles user-related actions
+class UsersController < ApplicationController
 
+    # Prepares a new user form
     def new
         @user = User.new
     end
 
+    # Processes user creation
     def create
         @user = User.new(user_params)
         if @user.save
+            # Redirect on successful save
             redirect_to @user, notice: 'User was successfully created'
         else
+            # Re-render form with errors
             render :new
         end
     end
 
     private 
+    
+    # Strong parameters for user creation
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
+# User model definition
 class User < ApplicationRecord
+    # Validate presence of username and email
     validates :username, :email, presence: true
+    
+    # Secure password functionality
     has_secure_password
-end
+  end
+  
